72061de16e79c2581f0f32ffa6f0e658563a8e2b
Jóhann Björn Björnsson <johann.bjorn.bjornsson@gmail.com>
2016-10-07 01:35:15 +0200

    Extension can now display status of wash rooms in both building 6 and 18. Various UI tweaks.

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 Google Chrome extension that shows the status of washing machines at
 Tingbjerg Kollegiet.
 
+## Packing
 To pack the extension into a Google Chrome extension (.crx file), open Google Chrome and type chrome://extensions/ in the URL bar. Tick "Developer mode" and click "Pack Extension". Choose the project's root folder and click "Pack Extension".
+
+## Permissions
+The extension asks permission to read and change data on two websites, 77.75.162.69 and 77.75.162.70 which are the websites that washing machine status is downloaded from.
--- popup.js
+++ popup.js
@@ -2,88 +2,128 @@ var machines = [];
 var columns = ["Machine", "Status", "Remaining time", "Start time"];
 var loading = document.getElementById("refresh");
 
-var refresh = function() {
-  var x = new XMLHttpRequest();
-  x.open('GET', "http://77.75.162.69/Status.asp");
-  x.responseType = 'document';
-  x.onload = function() {
-    machines = [];
+var washRoom6 = "77.75.162.69";
+var washRoom18 = "77.75.162.70";
 
-    //Extract raw data from dirty HTML
-    var response = x.response;
-    console.log(response);
-    var table = response.getElementsByTagName("table")[1];
-    var trs = table.getElementsByTagName("tr");
-    var skip = true;
-    for (var i = 0; i < trs.length; i++) {
-      if (skip) {
-        skip = false;
-        continue;
+var getWashRoomData = function() {
+  chrome.storage.sync.get("washRoom", function(result) {
+    var currentWashRoom = result.washRoom;
+
+    var x = new XMLHttpRequest();
+    switch (currentWashRoom) {
+      case 6:
+        x.open('GET', "http://"+washRoom6+"/Status.asp");
+        break;
+      case 18:
+        x.open('GET', "http://"+washRoom18+"/Status.asp");
+        break;
+      default:
+        x.open('GET', "http://"+washRoom6+"/Status.asp");
       }
-      var tr = trs[i];
-      tds = tr.getElementsByTagName("td")
 
-      machines.push({
-        0: tds[0].textContent.replace(/\s/g,'').replace(/VASK/g,'Washing machine ').replace(/TUMBLER/g,'Tumbler '),
-        //1: tds[1].textContent.replace(/\s/g,''),
-        1: tds[2].textContent.replace(/\s/g,'').replace(/Fri/g,'Free').replace(/Optaget/g,'Occupied'),
-        2: tds[3].textContent.replace(/\s/g,''),
-        3: tds[4].textContent.replace(/\s/g,'')
-      });
-    }
+    x.responseType = 'document';
+    x.onload = function() {
+      machines = [];
 
-    //Clean GUI
-    var tables = document.getElementsByTagName("table");
-    for (var i = 0; i < tables.length; i++) {
-      tables[i].remove();
-    }
+      //Extract raw data from dirty HTML
+      var response = x.response;
+      var table = response.getElementsByTagName("table")[1];
+      var trs = table.getElementsByTagName("tr");
+      var skip = true;
+      for (var i = 0; i < trs.length; i++) {
+        if (skip) {
+          skip = false;
+          continue;
+        }
+        var tr = trs[i];
+        tds = tr.getElementsByTagName("td")
 
-    //Populate GUI
-    var body = document.getElementsByTagName("body")[0];
-    var table = document.createElement("table");
-    var cellpadding = document.createAttribute("cellpadding");
-    cellpadding.value = 0;
-    var cellspacing = document.createAttribute("cellspacing");
-    cellspacing.value = 0;
-    table.setAttributeNode(cellpadding);
-    table.setAttributeNode(cellspacing);
+        machines.push({
+          0: tds[0].textContent.replace(/\s/g,'').replace(/VASK/gi,'Washing machine ').replace(/TUMBLER\s*/gi,'Tumbler '),
+          //1: tds[1].textContent.replace(/\s/g,''),
+          1: tds[2].textContent.replace(/\s/g,'').replace(/Fri/gi,'Free').replace(/Optaget/gi,'Occupied'),
+          2: tds[3].textContent.replace(/\s/g,''),
+          3: tds[4].textContent.replace(/\s/g,'')
+        });
+      }
 
-    //Start with the table header
-    var th = document.createElement("thead");
-    for (var i = 0; i < columns.length; i++) {
-      var td = document.createElement("td");
-      td.innerHTML = columns[i];
-      th.appendChild(td);
-    }
-    table.appendChild(th);
+      //Populate GUI
+      var body = document.getElementsByTagName("body")[0];
+      var table = document.createElement("table");
+      var cellpadding = document.createAttribute("cellpadding");
+      cellpadding.value = 0;
+      var cellspacing = document.createAttribute("cellspacing");
+      cellspacing.value = 0;
+      table.setAttributeNode(cellpadding);
+      table.setAttributeNode(cellspacing);
 
-    //Rest of data for table
-    for (var i = 0; i < machines.length; i++) {
-      var machine = machines[i];
-      var tr = document.createElement("tr");
-      for (var j = 0; j < Object.keys(machine).length; j++) {
+      //Start with the table header
+      var th = document.createElement("thead");
+      for (var i = 0; i < columns.length; i++) {
         var td = document.createElement("td");
-        td.innerHTML = machine[j];
-        tr.appendChild(td);
+        td.innerHTML = columns[i];
+        th.appendChild(td);
       }
-      table.appendChild(tr);
-    }
+      table.appendChild(th);
 
-    body.appendChild(table);
+      //Rest of data for table
+      for (var i = 0; i < machines.length; i++) {
+        var machine = machines[i];
+        var tr = document.createElement("tr");
+        for (var j = 0; j < Object.keys(machine).length; j++) {
+          var td = document.createElement("td");
+          td.innerHTML = machine[j];
+          tr.appendChild(td);
+        }
+        table.appendChild(tr);
+      }
 
-    //Hide the loading icon
-    loading.classList.remove("loading");
-  };
+      //Clean GUI and add the newly generated table
+      var tables = document.getElementsByTagName("table");
+      for (var i = 0; i < tables.length; i++) {
+        tables[i].remove();
+      }
+      body.appendChild(table);
 
-  x.onerror = function() {
-    console.log('Network error.');
-  };
+      //Update loading icon
+      loading.classList.remove("loading");
 
-  x.send();
-  //Show the loading icon while we wait for the data
-  loading.classList.add("loading");
+      //Update tab classes
+      for (tab of document.getElementsByClassName("tab")) {
+        tab.classList.remove("active");
+      }
+      document.getElementById("tab-building-"+currentWashRoom).classList.add("active");
+    };
+
+    x.onerror = function() {
+      console.log('Network error.');
+    };
+
+    x.send();
+
+    //Spin the loading icon while we wait for the data
+    loading.classList.add("loading");
+  });
 };
 
-document.addEventListener('DOMContentLoaded', refresh);
+var chooseWashroom = function (e) {
+  switch (e.target.id) {
+    case "tab-building-6":
+      chrome.storage.sync.set({'washRoom': 6});
+      getWashRoomData();
+      break;
+    case "tab-building-18":
+      chrome.storage.sync.set({'washRoom': 18});
+      getWashRoomData();
+      break;
+    default:
+      getWashRoomData();
+  }
+}
 
-document.getElementById("refresh").addEventListener('click', refresh);
+//Adding Event listeners
+document.addEventListener("DOMContentLoaded", chooseWashroom);
+document.getElementById("refresh").addEventListener('click', getWashRoomData);
+for (tab of document.getElementsByClassName("tab")) {
+  tab.addEventListener('click', chooseWashroom);
+}
